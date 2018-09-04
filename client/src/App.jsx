a771baa776c5ad68bf8fe7eb4a7667bf531d5b78
BrunoTorresF <bruno.torres.fabila@gmail.com>
2018-09-04 15:13:24 -0500

    update dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { hot } from 'react-hot-loader';
 
 class App extends Component {
   render() {
-    return <div>I to the Modern React Boilerplate</div>;
+    return <div>Put your phone away</div>;
   }
 }
 //hot export works with RHL. Remove when starting fullstack integration
