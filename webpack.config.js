46b32999a56fc1aa7b6ef23e1bbec594d95af204
BrunoTorresF <bruno.torres.fabila@gmail.com>
2019-03-05 18:34:52 -0600

    fix webpack config spacing

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: [
-    './client/src/Index.js'
-  ],
+  entry: ['./client/src/Index.js'],
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'client/dist')
+    path: path.join(__dirname, 'client/dist'),
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: [
-          'babel-loader'
-        ],
+        use: ['babel-loader'],
         exclude: /node_modules|packages/,
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: [
-          'eslint-loader'
-        ]
+        use: ['eslint-loader'],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-      }
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
 
   devServer: {
@@ -39,6 +33,6 @@ module.exports = {
     host: 'localhost',
     historyApiFallback: true,
     // respond to 404s with index.html
-    inline: true
-  }
+    inline: true,
+  },
 };
